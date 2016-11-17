15f58b9c137e8efdc2f433b52e5d9a02bc1cd82c
Caleb Jones <code@calebjones.net>
2016-11-16 21:05:48 -0500

    Add a simple network program

--- src/org/pihisamurai/network/Example.java
+++ src/org/pihisamurai/network/Example.java
@@ -1,4 +1,36 @@
 package org.pihisamurai.network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Example {
+	DatagramSocket socket;
+	public Example() throws SocketException {
+		socket = new DatagramSocket();
+	}
+	
+	public void request(String name) throws IOException {
+		InetAddress address = InetAddress.getByName("localhost");
+		byte[] buffer = name.getBytes();
+		DatagramPacket packet = new DatagramPacket(
+				buffer, buffer.length, address, 4242);
+		
+		// send the name to the server
+		socket.send(packet);
+	}
+	
+	public String getResponse() throws IOException {
+		// read the response
+		byte[] buffer = new byte[512];
+		DatagramPacket packet = new DatagramPacket(
+				buffer, buffer.length);
+		socket.receive(packet);
+
+		// return it
+		return new String(packet.getData())
+				.substring(0, packet.getLength());
+	}
 }
--- src/org/pihisamurai/network/ExampleTest.java
+++ src/org/pihisamurai/network/ExampleTest.java
@@ -2,13 +2,20 @@ package org.pihisamurai.network;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class ExampleTest {
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void testServerProtocol() throws IOException {
+		Example client = new Example();
+		ExampleServer server = new ExampleServer();
+		
+		client.request("Caleb");
+		server.respond();
+		assertEquals("Hello Caleb!", client.getResponse());
 	}
 
 }
